898621cb9fe1205b74612209827a4d8d02acc0e4
4Source <38220764+4Source@users.noreply.github.com>
2024-03-05 16:32:54 +0100

    Fix missing SettingsTab

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { Plugin } from 'obsidian';
 import { DEFAULT_SETTINGS, Settings } from './settings/SettingsInterface';
+import { MyPluginSettingTab } from './settings/SettingsTab';
 
 export default class MyPlugin extends Plugin {
 	settings: Settings;
 
 	async onload () {
 		await this.loadSettings();
+
+		// This adds a settings tab so the user can configure various aspects of the plugin
+		this.addSettingTab(new MyPluginSettingTab(this.app, this));
 	}
 
 	onunload () {
